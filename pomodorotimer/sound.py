1d8c4d4ef60e080af9b63ce28f8ab183f0f93c84
patsuckow <patsuckow@yandex.ru>
2023-12-10 22:26:27 +0300

    Update sound.py (10.12.2023):
    1. Used os.path.abspath and os.path.join for more reliable operation with the paths to the sound file.
    
    2. Renamed methods for more information: default_sound->play_default_sound, wav_sound->play_wav_sound.

--- pomodorotimer/sound.py
+++ pomodorotimer/sound.py
@@ -5,21 +5,20 @@ from pydub.playback import play
 
 class Sound:
     def __init__(self):
-        self.path = os.path.dirname(__file__) + "/tic-tic.wav"
+        self.path = os.path.abspath(os.path.join(os.path.dirname(__file__), "tic-tic.wav"))
         if os.path.exists(self.path):
-            self.wav_sound()
+            self.play_wav_sound()
         else:
-            self.default_sound()
+            self.play_default_sound()
 
     @staticmethod
-    def default_sound() -> None:
+    def play_default_sound() -> None:
         """
-        It will be used when the wav file is not been found.
-        Default: Frequency 432 Hz, duration=864 milliseconds.
+        Play a default sound: Frequency 432 Hz, duration=864 milliseconds.
         """
         play(generators.Sine(432).to_audio_segment(duration=864))
-
-    def wav_sound(self) -> None:
+    
+    def play_wav_sound(self) -> None:
         """Play wav file"""
         play(AudioSegment.from_wav(self.path))
 
