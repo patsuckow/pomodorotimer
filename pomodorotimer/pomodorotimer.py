d5479258e980b2d0a877672fe74db7e9b13030ef
patsuckow <patsuckow@yandex.ru>
2023-12-18 15:17:36 +0300

    Minor code changes

--- pomodorotimer/pomodorotimer.py
+++ pomodorotimer/pomodorotimer.py
@@ -1,12 +1,12 @@
-import os
 import sys
 import time
 import argparse
 import subprocess
 from collections import deque
 from .graph_statistic import graph
+from .sound import Sound as play_sound
 from .statistics_db import (
-    today_entry,
+    save_today_entry,
     read_all_entry,
     read_today_entry,
     delete_today_entry,
@@ -26,13 +26,22 @@ class PomodoroTimer:
     CLI Pomodoro Timer - https://github.com/patsuckow/pomodorotimer
     """
     def __init__(self) -> None:
+        self.mess = "Input min: 25/5/15/30 for your Pomodoro, or 0 for exit: "
+        self.mess_two = "\nPomodoro's timer work has been interrupted."
+        self.mess_three = "One Pomodoro cycle is over."
         self.scale_progress = ""
         self.clear_up_str = f"\x1b[A{79*' '}\r"
         self.stack_watch = deque(
             list("🕛🕧🕐🕜🕑🕝🕒🕞🕓🕟🕔🕠🕕🕡🕖🕢🕗🕣🕘🕤🕙🕥🕚🕦")
         )
+        self.process_argument()
+        self.pomodoro_setup()
 
-        # Check if the argument was passed to get/remove statistics
+    def process_argument(self) -> None:
+        """
+        Check if the correct one for working with statistics is passed;
+        And depending on this, we make requests to the database or not.
+        """
         argument = self.arg()
         if argument in ["today", "all", "delete-today", "delete-all"]:
             if argument == "today":
@@ -45,38 +54,38 @@ class PomodoroTimer:
                 delete_today_entry()
             elif argument == "delete-all":
                 delete_all_entry()
+
             close_db()
-            exit(0)
-        else:
-            self.clear_console_before_pomodoro_setup()
+            self.stop_pomodoro()
 
-            mess = "Input min: 25/5/15/30 for your Pomodoro, or 0 for exit: "
-            mess_two = "\nPomodoro's timer work has been interrupted."
-            try:
-                while True:
-                    try:
-                        self.min = int(input(mess))
-                    except ValueError:
-                        self.write_flush(self.clear_up_str)
-                        continue
+    def pomodoro_setup(self) -> None:
+        self.clear_console()
+
+        try:
+            while True:
+                try:
+                    self.show_console_cursor()
+                    self.min = int(input(self.mess))
+                except ValueError:
                     self.write_flush(self.clear_up_str)
-                    if self.min in [5, 15, 25, 30]:
-                        self.run_pomodoro()
-                    elif self.min == 0:
-                        self.write_flush(self.clear_up_str)
-                        if self.scale_progress != "":
-                            print(self.scale_progress + mess_two)
-                        exit(0)
-            except KeyboardInterrupt:
-                # interrupt processing (Ctrl + C)
-                self.show_console_cursor()
-                print(mess_two)
-                exit(0)
+                    continue
 
-        self.show_console_cursor()
+                self.write_flush(self.clear_up_str)
+
+                if self.min in [5, 15, 25, 30]:
+                    self.run_pomodoro()
+                elif self.min == 0:
+                    self.write_flush(self.clear_up_str)
+                    if self.scale_progress != "":
+                        print(self.scale_progress + self.mess_two)
+                    self.stop_pomodoro()
+        except KeyboardInterrupt:
+            # interrupt processing (Ctrl + C)
+            print(self.mess_two)
+            self.stop_pomodoro()
 
     @staticmethod
-    def clear_console_before_pomodoro_setup() -> None:
+    def clear_console() -> None:
         if sys.platform in ("linux", "darwin"):
             subprocess.call("clear", shell=True)
         elif sys.platform in ("win32", "cygwin"):
@@ -96,9 +105,9 @@ class PomodoroTimer:
 
         return parser.parse_args().statistic
 
-    def bar_pomodoro(self) -> None:
+    def progress_scale(self) -> str:
         """
-        Colorize the timer scale for pomodoro in a different color, depending on 
+        Colorize the timer scale for pomodoro in a different color, depending on
         the set time.
         """
         match self.min:
@@ -110,21 +119,32 @@ class PomodoroTimer:
                 self.scale_progress += f"\x1b[41m\x1b[1m   {self.min}   \x1b[0m"
             case 30:
                 self.scale_progress += f"\x1b[42m\x1b[1m    {self.min}    \x1b[0m"
-        
+
         self.write_flush(self.clear_up_str)
-        print(self.scale_progress)
 
+        return self.scale_progress
 
-    def reverse_timer(self) -> None:
+    def run_reverse_timer(self) -> None:
         """Countdown"""
         self.write_flush("\n" + self.clear_up_str)
         for i in range(self.min * 60, 0, -1):
             self.write_flush(f"\r{self.animated_clock()}{self.lead_time(i)} ")
+            # The code execution delay (by 1 second) allows you to change the display
+            #  of a string with a timer every second, t.e. this is the timer itself.
+            # (For yourself: If you change the code, then for quick checking of 
+            # performance, comment on this line and the code will be executed instantly)
             time.sleep(1)
         self.write_flush(f"\r{35*' '}\r")
 
     @staticmethod
     def write_flush(string: str) -> None:
+        """
+        The sys.stdout.flush() method is needed to clear the buffer output of
+        the standard output stream. T.e. all data that has been delayed in the
+        output buffer will be sent to the output device (for example, to the console)
+        immediately. This is useful when you need to make sure that all the data was
+        printed before the code continues to execute.
+        """
         sys.stdout.write(string)
         sys.stdout.flush()
 
@@ -164,38 +184,7 @@ class PomodoroTimer:
 
         return clock
 
-    @staticmethod
-    def create_and_kill_background_process() -> None:
-        """
-        In order to hide the output of debugging information of the audio
-        player, we will transfer the playback/generation code of the audio
-        to a separate file and we will call this script in the background.
-        Why so? - because standard output redirection does not work due to
-        the specifics of ffmpeg it self.
-
-        The process itself is needed to play the WAV file or the generated
-        sound.
-        """
-        try:
-            sound_py = os.path.join(os.path.dirname(__file__), "sound.py")
-            if not os.path.exists(sound_py):
-                raise FileExistsError
-
-            pid = subprocess.Popen(
-                [sys.executable, sound_py],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            ).pid
-
-            os.kill(pid, 0)
-        except FileExistsError:
-            print("File sound.py not fund\n")
-        except ProcessLookupError:
-            print("No such process\n")
-        except PermissionError:
-            print("Operation not permitted (i.e., process exists)\n")
-
-    def logging_statistics(self) -> None:
+    def save_statistics(self) -> None:
         """INSERT / UPDATE log statistic in DB
 
         According to the principle of the Pomodoro technique - if you are
@@ -216,11 +205,12 @@ class PomodoroTimer:
             relaxation = self.min
         else:
             work = self.min
-        today_entry(work, relaxation)
+        save_today_entry(work, relaxation)
 
-    def send_notification(self):
+    def show_pop_up_notice(self):
         """
-        Push-notifications on Linux/macOS desktop
+        Show pop-up notification on Linux/macOS desktop.
+        (Windows OS does not support the display of pop-up notifications.)
         """
         title = "⏰ Pomodoro: "
         info = f"{self.min} min TIME'S UP!"
@@ -228,29 +218,41 @@ class PomodoroTimer:
         if sys.platform.startswith("linux"):
             command = ["notify-send", title, info]
         elif sys.platform == "darwin":  # macOS
-            command = ["osascript", "-e", f'display notification "{info}" with title "{title}"']
+            command = [
+                "osascript",
+                "-e",
+                f'display notification "{info}" with title "{title}"',
+            ]
         else:
             return  # Do nothing if not on supported platform
 
         subprocess.run(command, check=False)
-    
+
     def check_one_pomodoro_cycle(self) -> None:
         """
         Checking the end of the pomodoro cycle
         """
         if self.min in [15, 30]:
-            time.sleep(4)
-            print("One Pomodoro cycle is over.\n")
-            self.show_console_cursor()
-            exit(0)
+            return print(self.mess_three), self.stop_pomodoro()
 
     def run_pomodoro(self) -> None:
         """Run pomodoro cicle"""
+        # Нide the cursor display from the console
         self.hide_console_cursor()
-        self.reverse_timer()
-        self.create_and_kill_background_process()
-        self.bar_pomodoro()
-        self.logging_statistics()
-        self.send_notification()
+        # Start the countdown timer
+        self.run_reverse_timer()
+        # Display (update display) the pomodoro progress bar
+        print(self.progress_scale())
+        # At the end of the timer, play the sound signal
+        self.show_pop_up_notice()
+        # Save statistics in the database
+        play_sound()
+        # Show pop-up notice notification
+        self.save_statistics()
+        # Check if the latter introduced 15 or 30, then we complete 
+        # the pomodoro execution cycle
         self.check_one_pomodoro_cycle()
+    
+    def stop_pomodoro(self) -> None:
         self.show_console_cursor()
+        exit(0)
