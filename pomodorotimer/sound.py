61a56b2d3be359bed71e5df6cf711c7095271095
patsuckow <patsuckow@yandex.ru>
2023-12-18 15:13:48 +0300

    Changed sound.py so that it does not play as a separate process, but connects as a user module to pomodorotimer.py.
    For this, libraries were used as new dependencies: sunddevice, numpy and scipy.

--- pomodorotimer/sound.py
+++ pomodorotimer/sound.py
@@ -1,7 +1,7 @@
 import os
-from pydub import AudioSegment, generators
-from pydub.playback import play
-
+import sounddevice as sd
+import numpy as np
+from scipy.io import wavfile
 
 class Sound:
     def __init__(self):
@@ -14,14 +14,27 @@ class Sound:
     @staticmethod
     def play_default_sound() -> None:
         """
-        Play a default sound: Frequency 432 Hz, duration=864 milliseconds.
+        Sound generation with a frequency of 432 Hz and a duration of 864 milliseconds
         """
-        play(generators.Sine(432).to_audio_segment(duration=864))
+        frequency = 432
+        duration = 0.864
+        # Timeline (44100 Hz - sampling rate)
+        t = np.linspace(0, duration, int(44100 * duration), endpoint=False)
+        # Generation of sinusoidal sound
+        data = 0.5 * np.sin(2 * np.pi * frequency * t)
+        # Playback
+        sd.play(data, 44100)
+        # Waiting for the playback to end
+        sd.wait()
     
     def play_wav_sound(self) -> None:
         """Play wav file"""
-        play(AudioSegment.from_wav(self.path))
-
+        # Reading audio file
+        fs, data = wavfile.read(self.path)
+        # Playback
+        sd.play(data, fs)
+        # Waiting for the playback to end
+        sd.wait()
 
 if __name__ == "__main__":
     Sound()
